681d1aee87d176a01048b9d980a0653aa1fc48d0
Nuria <nuria.villaronga@xercode.com>
2022-01-17 17:58:46 +0100

    changes validation

--- js/validationStyle.js
+++ js/validationStyle.js
@@ -1,43 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    var errMessage = document.getElementById('errMessage');
+    var errMessage = document.querySelector('.errMessage');
     var form = document.querySelector('form');
     var inputsValidation = document.querySelectorAll('.requiredInput');
     var invalidMessages = document.querySelectorAll('.invalid-feedback');
-    var recordReferenceM = document.getElementById('recordReferenceM');
-    var isbn13M = document.getElementById('isbn13M');
-    var eanM = document.getElementById('eanM');
-    var publishingDateM = document.getElementById('publishingDateM');
-
-    function displayMessages(input, recordReferenceM, eanM, isbn13M, publishingDateM) {
-        if (input.getAttribute('name') === "recordReference") {
-            recordReferenceM.setAttribute("style", "display: block");
-        }
-        else if (input.getAttribute('name') === "ean") {
-            eanM.setAttribute("style", "display: block");
-        }
-        else if (input.getAttribute('name') === "isbn13") {
-            isbn13M.setAttribute("style", "display: block");
-        }
-        else if (input.getAttribute('name') === "publishingDate") {
-            publishingDateM.setAttribute("style", "display: block");
-        }
-    }
-
-    function hiddeMessages(input, recordReferenceM, eanM, isbn13M, publishingDateM) {
-        if (input.getAttribute('name') === "recordReference") {
-            recordReferenceM.setAttribute("style", "display: none");
-        }
-        else if (input.getAttribute('name') === "ean") {
-            eanM.setAttribute("style", "display: none");
-        }
-        else if (input.getAttribute('name') === "isbn13") {
-            isbn13M.setAttribute("style", "display: none");
-        }
-        else if (input.getAttribute('name') === "publishingDate") {
-            publishingDateM.setAttribute("style", "display: none");
-        }
-    }
 
     function hiddeAllMessages(invalidMessages) {
         invalidMessages.forEach((invalidMessage) => {
@@ -53,59 +19,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
         input.setAttribute("style", "border-bottom: 1px solid rgb(185, 185, 185);");
     }
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault(); //Para que los datos enviados no aparezcan en la ruta
-        errMessage.setAttribute("style", "visibility: visible; display: block;");
+    form.addEventListener('submit', () => {
+        //e.preventDefault();
+        
         inputsValidation.forEach((input) => {
+            errMessage.setAttribute("style", "visibility: visible; display: block;");
             if (input.value === "") {
                 failInput(input);
+                document.getElementById(input.getAttribute("data-bs-target")).style.display="block";
             }
-            input.addEventListener('change', () => {
+            input.addEventListener('keyup', () => {
                 if (input.value !== "") {
                     correctInput(input);
+                    document.getElementById(input.getAttribute("data-bs-target")).style.display="none";
                 }
                 else {
                     failInput(input);
+                    document.getElementById(input.getAttribute("data-bs-target")).style.display="block";
                 }
             });
         });
+    });
+
+    //Hay que hacer un evento para el reset porque sino, cuando pulses en reset no se eliminan los mensajes de error del input
+    form.addEventListener('reset', () => {
+        errMessage.setAttribute("style", "visibility: hidden; display: none;");
         
-        form.addEventListener('reset', () => {
-            errMessage.setAttribute("style", "visibility: hidden; display: none;");
+        inputsValidation.forEach((input) => {
+            correctInput(input);
             
-            inputsValidation.forEach((input) => {
+            input.addEventListener('change', () => {
                 correctInput(input);
-                
-                input.addEventListener('change', () => {
-                    correctInput(input);
-                });
-                
-                input.addEventListener('keyup', () => {
-                    correctInput(input);
-                    hiddeAllMessages(invalidMessages)
-                });
-                
-                hiddeAllMessages(invalidMessages)
-                
-                form.addEventListener('submit', () => {
-                    inputsValidation.forEach((input) => {
-                        if (input.value === "") {
-                            failInput(input);
-                            displayMessages(input, recordReferenceM, eanM, isbn13M, publishingDateM);
-                        }
-                        input.addEventListener('keyup', () => {
-                            if (input.value !== "") {
-                                correctInput(input);
-                                hiddeMessages(input, recordReferenceM, eanM, isbn13M, publishingDateM);
-                            }
-                            else {
-                                failInput(input);
-                                displayMessages(input, recordReferenceM, eanM, isbn13M, publishingDateM);
-                            }
-                        });
-                    });
-                });
             });
+            
+            input.addEventListener('keyup', () => {
+                correctInput(input);
+                hiddeAllMessages(invalidMessages)
+            });
+            
+            hiddeAllMessages(invalidMessages)
         });
     });
 
