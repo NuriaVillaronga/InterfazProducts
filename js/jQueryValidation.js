3aa856925e42ae94696237a1e8777a4b7cd16e51
Nuria <nuria.villaronga@gmail.com>
2022-01-25 19:52:18 +0100

    changes on publishingDate validation

--- js/jQueryValidation.js
+++ js/jQueryValidation.js
@@ -24,7 +24,12 @@ $(function() {
 
     $.validator.addMethod( "isbn10Option", function( value, element ) {
         return this.optional( element ) || value === "02";
-    }, "El ISBN10 solo puede tomar el valor 02" );
+    }, "El ISBN10 solo puede tomar el valor 02");
+
+    $.validator.addMethod( "publishingDateFormat", function( value, element ) {
+        return this.optional( element ) || 
+            /^(?:(0{2,3}[1-9]|0{1,2}[1-9]\d|0[1-9]\d{2}|[1-9]\d{3})(?:(0[1-9]|1[0-2])(0[1-9]|1\d|2[0-8])|(0[13-9]|1[0-2])(29|30)|(0[13578]|1[02])(31))|(\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[13579][26]|[2468][048])00)(0?2)(29))$/.test( value );
+    }, "Fecha de publicación debe contener una fecha válida en formato AAAAMMDD");
 
     $(".dataForm").validate({
         rules: {
@@ -43,7 +48,8 @@ $(function() {
                 isbn10Option : true
             },
             publishingDate: {
-                required : true
+                required : true,
+                publishingDateFormat : true
             }
         },
         messages: {
