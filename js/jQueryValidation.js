f10723ea9ae7a396b87c14ed5c76797e40334216
Nuria <nuria.villaronga@xercode.com>
2022-01-26 17:15:17 +0100

    changes on validations

--- js/jQueryValidation.js
+++ js/jQueryValidation.js
@@ -13,23 +13,34 @@ $(function() {
             .addBack().css( "border-color", "rgb(185, 185, 185)" ); //Cambiar al color necesario
         },
       });
-
+      
     $.validator.addMethod( "isbn13Option", function( value, element ) {
-        return this.optional( element ) || value === "15";
-    }, "El ISBN13 solo puede tomar el valor 15" );
-
-    $.validator.addMethod( "eanOption", function( value, element ) {
-        return this.optional( element ) || value === "03";
-    }, "El EAN solo puede tomar el valor 03" );
+        return this.optional( element ) || 
+            /^(?=[0-9]{13}$)97[89][]?[0-9]{1,5}?[0-9]?[0-9]+[]?$/.test( value );
+    }, "El ISBN13 debe contener <u>13 dígitos</u> además de empezar por <em>978</em> o <em>979</em>");
 
     $.validator.addMethod( "isbn10Option", function( value, element ) {
-        return this.optional( element ) || value === "02";
-    }, "El ISBN10 solo puede tomar el valor 02");
+        return this.optional( element ) || 
+            /^(?=[0-9X]{10}$)[0-9]?[0-9]+[]?[0-9X]$/.test( value );
+    }, "El ISBN10 debe contener <u>10 dígitos</u>");
+
+    $.validator.addMethod( "eanOption", function( value, element ) {
+        return this.optional( element ) || 
+            /^(?=[0-9X]{13}$)[0-9]?[0-9]+[]?[0-9X]$/.test( value );
+    }, "El EAN debe contener <u>13 dígitos</u>");
 
     $.validator.addMethod( "publishingDateFormat", function( value, element ) {
         return this.optional( element ) || 
             /^(?:(0{2,3}[1-9]|0{1,2}[1-9]\d|0[1-9]\d{2}|[1-9]\d{3})(?:(0[1-9]|1[0-2])(0[1-9]|1\d|2[0-8])|(0[13-9]|1[0-2])(29|30)|(0[13578]|1[02])(31))|(\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[13579][26]|[2468][048])00)(0?2)(29))$/.test( value );
-    }, "Fecha de publicación debe contener una fecha válida en formato AAAAMMDD");
+    }, "Introduce una fecha válida en formato AAAAMMDD");
+
+    $.validator.addMethod( "editionNumberType", function( value, element ) {
+        return this.optional( element ) || value >= 0 && Number.isInteger(Number(value));
+    }, "Introduce un número de edición válido");
+
+    $.validator.addMethod( "numberPagesType", function( value, element ) {
+        return this.optional( element ) || value >= 0 && Number.isInteger(Number(value));
+    }, "Introduce un número de páginas válido");
 
     $(".dataForm").validate({
         rules: {
@@ -42,7 +53,7 @@ $(function() {
             },
             ean: {
                 required : true,
-                eanOption: true
+                eanOption : true
             },
             isbn10: {
                 isbn10Option : true
@@ -50,6 +61,12 @@ $(function() {
             publishingDate: {
                 required : true,
                 publishingDateFormat : true
+            },
+            editionNumber: {
+                editionNumberType : true
+            },
+            numberPages: {
+                numberPagesType : true
             }
         },
         messages: {
